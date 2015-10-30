6449b2d4c14a5ad920a84b285493a7d220f8bb4f
Josh Harrison <joshharr1997@gmail.com>
2015-10-30 12:16:16 -0400

    test

---
+++ ConsoleApplication1/ConsoleApplication1/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnemyNameSpace
+{
+    class Enemy
+    {
+        int health;
+        public string name;
+        public bool isRunning;
+        public int Health
+        {
+            get { return health; }
+            set { health = value; }
+        }
+        public Enemy()
+        {
+            health = 100;
+            name = "Kwame";
+            isRunning = true;
+        }
+        Enemy(int eHealth, string eName, bool running)
+        {
+            health = eHealth;
+            name = eName;
+            isRunning = running;
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnemyNameSpace;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variable for stopping the while loop
+            int stopValue = 1;
+            Enemy myEnemy = new Enemy();
+            Enemy.myEnemy2 = new Enemy(23, "Joe Enemy", true);
+            // players health variable
+            int playerHealth = 100;
+            // construct the while loop
+            while (stopValue != 0)
+            {
+                Console.WriteLine("Enter 1 to continue or 0 to exit");
+                string toStringValue = Console.ReadLine();
+                int.TryParse(toStringValue, out stopValue);
+                addDamageToPlayer(ref playerHealth);
+            }
+        }
+        public static void addDamageToPlayer(ref int Health)
+        {
+            Health--;
+            Console.WriteLine("{0}", Health);
+        }
+        public static string returnEnemyName(string name)
+        {
+            return name;
+        }
+        public static Enemy returnNewEnemy(Enemy enemy)
+        {
+            return enemy;
+        }
+    }
+
+}
