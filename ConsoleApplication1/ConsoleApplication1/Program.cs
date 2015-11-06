b84a37c9eb2ccb9ce962ab416b5a94a528919eac
Josh Harrison <joshharr1997@gmail.com>
2015-11-06 12:07:03 -0500

    11/06/2015
    Added TimeStep (sort of) more comment

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -17,15 +17,27 @@ namespace ConsoleApplication1
             Enemy myEnemy2 = new Enemy(23, "Joe Enemy", true);
             // players health variable
             int playerHealth = 100;
+            // 
+            int timeStep = 60;
+            int someCrazyTimeStep = 1000;
             // construct the while loop
             while (stopValue != 0)
             {
+                // grab the time step
+                // if the returned time step is greater than our locked time step set it back
+                if(someCrazyTimeStep > timeStep)
+                {
+                    // set it back
+                    someCrazyTimeStep = timeStep;
+                }
+                // 
                 Console.WriteLine("Enter 1 to continue or 0 to exit");
                 string toStringValue = Console.ReadLine();
                 int.TryParse(toStringValue, out stopValue);
                 addDamageToPlayer(ref playerHealth);
             }
         }
+        // Pass by Value and Pass by Reference
         public static void addDamageToPlayer(ref int Health)
         {
             Health--;
